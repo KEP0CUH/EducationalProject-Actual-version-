35ac3dc2c4a7cade77b5807253a62b1c9ba77c3b
KEPOCUH <tolik2001323@yandex.ru>
2022-10-04 11:54:09 +0300

    Restored shop

--- Assets/Scripts/PlanetSystem/PlanetInside.cs
+++ Assets/Scripts/PlanetSystem/PlanetInside.cs
@@ -4,42 +4,19 @@ using UnityEngine.UI;
 public class PlanetInside : MonoBehaviour
 {
     private PlanetData data;
-    private PlanetState state;
 
     [SerializeField] private Button riseButton;
     [SerializeField] private Button shopButton;
     [SerializeField] private Image background;
     [SerializeField] private Shop shop;
-    public void Init(PlanetState state)
-    {
-        this.state = state;
-        this.background.sprite = state.Data.IconBackground;
-        this.gameObject.SetActive(true);
-
-        this.riseButton.onClick.AddListener(Rise);
-        this.shopButton.onClick.AddListener(OpenShop);
-
-        if (shop != null)
-        {
-            shop.gameObject.SetActive(!shop.gameObject.activeInHierarchy);
-            shop.Init(state.Shop);
-        }
-    }
-
     public void Init(PlanetData data)
     {
         this.data = data;
-        this.background.sprite = state.Data.IconBackground;
+        this.background.sprite = data.IconBackground;
         this.gameObject.SetActive(true);
 
         this.riseButton.onClick.AddListener(Rise);
         this.shopButton.onClick.AddListener(OpenShop);
-
-        if (shop != null)
-        {
-            shop.gameObject.SetActive(!shop.gameObject.activeInHierarchy);
-            shop.Init(data.ShopKind);
-        }
     }
 
     public void AddPlayerInventory(Inventory inventory)
@@ -59,7 +36,7 @@ public class PlanetInside : MonoBehaviour
         if(shop != null)
         {
             shop.gameObject.SetActive(!shop.gameObject.activeInHierarchy);
-            shop.Init(state.Shop);
+            shop.Init(data.ShopKind);
         }
     }
 }
--- Assets/Scripts/PlanetSystem/PlanetState.cs
+++ Assets/Scripts/PlanetSystem/PlanetState.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class PlanetState
 {
     private PlanetData data;
-    private Shop shop;
     public PlanetData Data => data;
-    public Shop Shop => shop;
 
     public PlanetState(PlanetKind kind)
     {
         var planetDataList = Resources.Load<PlanetDataList>("ScriptableObjects/Planets/_PlanetList");
-        
+
         this.data = planetDataList.DownloadData(kind);
-        this.shop.Init(data.ShopKind);
     }
 }
